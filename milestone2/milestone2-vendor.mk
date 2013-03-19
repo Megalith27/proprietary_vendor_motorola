243fcec875eb887ddb927fd003c9a0fa6aeedae5
Megalith27 <perbin27@yahoo.com>
2013-03-19 22:59:49 +0400

    Update milestone2-vendor.mk

--- milestone2/milestone2-vendor.mk
+++ milestone2/milestone2-vendor.mk
@@ -163,9 +163,9 @@ PRODUCT_COPY_FILES += \
 
 #app
 #PRODUCT_COPY_FILES += \
-	$(vendor_path)/app/MotoPhonePortal.apk:system/app/MotoPhonePortal.apk \
-	$(vendor_path)/app/AudioEffectSettings.apk:system/app/AudioEffectSettings.apk \
-	$(vendor_path)/app/CompassCalibrate.apk:system/app/CompassCalibrate.apk
+#	$(vendor_path)/app/MotoPhonePortal.apk:system/app/MotoPhonePortal.apk \
+#	$(vendor_path)/app/AudioEffectSettings.apk:system/app/AudioEffectSettings.apk \
+#	$(vendor_path)/app/CompassCalibrate.apk:system/app/CompassCalibrate.apk
 
 #extra
 PRODUCT_COPY_FILES += \
